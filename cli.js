7c7ff754d80aac3b2aa61cdb5d2aaf77e0e006b3
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2020-01-28 14:34:52 +0200

    update(cli): add test command

--- cli.js
+++ cli.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const commander = require('commander');
 const getImage = require('./src/commands/getPicturesFromAemFolders');
 const { version } = require('./package.json');
@@ -18,4 +17,12 @@ commander
 		console.log(chalk.white.bgRed.bold("Misha vse ZBS!!! Gulayem :D."));
 	});
 
-commander.parse(process.argv);
+commander	
+	.command('test')
+	.description('test')
+	.action(function () {
+		console.log(chalk.white.bgRed.bold("test"));
+	});
+
+commander
+	.parse(process.argv);
