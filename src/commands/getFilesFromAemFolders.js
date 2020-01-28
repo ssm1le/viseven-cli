2da8bb7eff53a839c3ab0507995be63fbff2cbf2
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2020-01-28 17:56:44 +0200

    updare(getFilesFromAemFolders): refactoring

--- src/commands/getFilesFromAemFolders.js
+++ src/commands/getFilesFromAemFolders.js
@@ -1,18 +1,18 @@
 const fs = require('fs-extra')
-const path = require('path')
+const { sep } = require('path')
 const { getAllFoldersFromPath, moveFileToDirectory } = require('../modules/utils.js');
 
 module.exports = function (pathFrom, pathTo) {
     const foldersArray = getAllFoldersFromPath(pathFrom);
 
     foldersArray.forEach(folder => {
-        const pathToFolder = pathFrom + path.sep + folder;
+        const pathToFolder = pathFrom + sep + folder;
         const tempFolderName = `${pathToFolder}_temp`;
 
         fs.renameSync(pathToFolder, tempFolderName);
         const folderFiles = fs.readdirSync(tempFolderName);
         folderFiles.forEach(file => {
-            moveFileToDirectory(tempFolderName + path.sep + file, pathTo);
+            moveFileToDirectory(tempFolderName + sep + file, pathTo);
             fs.removeSync(tempFolderName);
         });
     });
