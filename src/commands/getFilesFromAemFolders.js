865b9f1f1e42454503380e38c26156186366343f
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2020-01-29 09:14:35 +0200

    update(*): refactoring

--- src/commands/getFilesFromAemFolders.js
+++ src/commands/getFilesFromAemFolders.js
@@ -1,18 +1,18 @@
 const fs = require('fs-extra')
-const { sep } = require('path')
+const { join } = require('path')
 const { getAllFoldersFromPath, moveFileToDirectory } = require('../modules/utils.js');
 
 module.exports = function (pathFrom, pathTo) {
     const foldersArray = getAllFoldersFromPath(pathFrom);
 
     foldersArray.forEach(folder => {
-        const pathToFolder = pathFrom + sep + folder;
-        const tempFolderName = `${pathToFolder}_temp`;
+        const pathToFolder = join(pathFrom, folder);
+        const tempFolderName = pathToFolder + "_temp";
 
         fs.renameSync(pathToFolder, tempFolderName);
         const folderFiles = fs.readdirSync(tempFolderName);
         folderFiles.forEach(file => {
-            moveFileToDirectory(tempFolderName + sep + file, pathTo);
+            moveFileToDirectory(join(tempFolderName, file), pathTo);
             fs.removeSync(tempFolderName);
         });
     });
--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,6 +1,5 @@
 const fs = require('fs-extra');
 const path = require('path');
-const chalk = require('chalk');
 
 exports.getAllFoldersFromPath = function (mainPath) {
   return fs.readdirSync(mainPath).filter(f => fs.statSync(path.join(mainPath, f)).isDirectory());
@@ -10,10 +9,9 @@ exports.moveFileToDirectory = function (file, pathToDirectory) {
   fs.renameSync(file, path.resolve(pathToDirectory, path.basename(file)));
 };
 
-exports.checkPath = function (path) {
-  if (fs.existsSync(path)) {
-    return path;
+exports.checkPath = function (pathForCheck) {
+  if (fs.existsSync(pathForCheck)) {
+    return pathForCheck;
   }
-  throw `Err! Invalid path: ${path}`;
-  return '';
+  throw `Err! Invalid path: ${pathForCheck}`;
 }
