a820e5c0e2222f09e2d44e67bc45d4623d41c813
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2020-02-03 09:09:21 +0200

    update(setConfig): modules

--- src/commands/setConfig.js
+++ src/commands/setConfig.js
@@ -1,11 +1,11 @@
-import fs from 'fs-extra';
-import path from 'path';
+import { writeFileSync } from 'fs-extra';
+import { resolve } from 'path';
 import { getConfigFile } from '../modules/utils';
 
 export function setApiKey(key) {
     const config = getConfigFile();
     const apiObj = getConfigObj(key);
-    fs.writeFileSync(path.join(__dirname, "..", "config.json"), JSON.stringify({ ...config, ...apiObj}));
+    writeFileSync(resolve(__dirname, "../config.json"), JSON.stringify({ ...config, ...apiObj }));
 }
 
 function getConfigObj(key) {
