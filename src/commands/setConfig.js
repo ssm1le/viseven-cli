e678c401223f985be78eec820884bc8a86bee850
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2020-02-04 16:17:14 +0200

    create class for config

--- src/commands/setConfig.js
+++ src/commands/setConfig.js
@@ -1,15 +1,6 @@
-import { writeFileSync } from 'fs-extra';
-import { resolve } from 'path';
-import { getConfigFile } from '../modules/utils';
+const Config = require('../modules/config').Config;
 
 export function setApiKey(key) {
-    const config = getConfigFile();
-    const apiObj = getConfigObj(key);
-    writeFileSync(resolve(__dirname, "../config.json"), JSON.stringify({ ...config, ...apiObj }));
-}
-
-function getConfigObj(key) {
-    return {
-        "apiKey": key
-    };
+    const conf = new Config();
+    conf.setApiKey(key);
 }
--- src/modules/config/index.js
+++ src/modules/config/index.js
@@ -1,8 +1,24 @@
-
-class Config {
+import fs from 'fs-extra';
+import path from 'path';
+const DEFAULT_CONFIG_PATH = path.resolve(__dirname, '../../config.json')
+export class Config {
     constructor() {
 
     }
 
-    
+    getConfigFile() {
+        return JSON.parse(fs.readFileSync(DEFAULT_CONFIG_PATH, 'utf8'));
+    }
+
+    getConfigObj(key) {
+        return {
+            "apiKey": key
+        }
+    }
+
+    setApiKey(key) {
+        const config = this.getConfigFile();
+        const apiObj = this.getConfigObj(key);
+        fs.writeFileSync(DEFAULT_CONFIG_PATH, JSON.stringify({ ...config, ...apiObj }));
+    }
 }
