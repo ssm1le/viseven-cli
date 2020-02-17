bac4e4b43c255b7a6e89697a0275acd3796186e6
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2020-02-17 12:14:49 +0200

    update(utils): add improvement

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,12 +1,14 @@
 import fs from 'fs-extra';
 import path from 'path';
+import glob from 'glob';
 
 export function getAllFoldersFromPath(mainPath) {
   return fs.readdirSync(mainPath).filter(f => fs.statSync(path.join(mainPath, f)).isDirectory());
 }
 
 export function getImagesFilesFromFolder(pathToFolder) {
-  return fs.readdirSync(pathToFolder, 'utf8').filter(fn => fn.endsWith('.png') || fn.endsWith('.jpg'));
+  const isDirectory = source => fs.lstatSync(source).isDirectory();
+  return glob.sync(`${pathToFolder}${path.sep}**${path.sep}*.{png,jpg}`).filter(itemPath => !isDirectory(itemPath));
 }
 
 export function moveFileToDirectory(file, pathToDirectory) {
