daaaf7c48bdd9b35dff98cf97c8b970c72abaa22
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2020-03-03 14:08:07 +0200

    update(get pdf info): add

--- src/commands/pdfToImage.js
+++ src/commands/pdfToImage.js
@@ -1,41 +1,13 @@
-const PDF2Pic = require("pdf2pic");
 import fs from 'fs-extra';
-const pdf = require('pdf-parse');
+import { PDFDocument } from 'pdf-lib'
 
-export function convert(path) {
-    console.log(path);
-    
-    let dataBuffer = fs.readFileSync(path);
+export async function convert(path) {
 
-    pdf(dataBuffer).then(function (data) {
+    const dataBuffer = await fs.readFile(path);
+    const pdfDoc = await PDFDocument.load(dataBuffer);
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
+    const { width, height } = firstPage.getSize();
 
-        // number of pages
-        // console.log(data.numpages);
-        // number of rendered pages
-        // console.log(data.numrender);
-        // PDF info
-        console.log(data.info);
-        // PDF metadata
-        // console.log(data.metadata);
-        // PDF.js version
-        // check https://mozilla.github.io/pdf.js/getting_started/
-        // console.log(data.version);
-        // PDF text
-        // console.log(data.text);
-
-    });
-
-    // const pdf2pic = new PDF2Pic({
-    //     density: 100,           // output pixels per inch
-    //     savename: "untitled",   // output file name
-    //     savedir: "./images",    // output file location
-    //     format: "jpg",          // output file format
-    //     size: "600x600"         // output size in pixels
-    // });
-
-    // pdf2pic.convertBulk("path/to/pdf/sample.pdf", -1).then((resolve) => {
-    //     console.log("image converter successfully!");
-
-    //     return resolve;
-    // });
+    console.log(`Width: ${width}px | Height: ${height}px`);
 }
