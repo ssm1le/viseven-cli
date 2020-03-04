c203e9bb65489be63dfe40530c74bc39f8219669
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2020-03-04 11:36:53 +0200

    fix

--- src/commands/pdfToImage.js
+++ src/commands/pdfToImage.js
@@ -1,23 +1,30 @@
 import fs from 'fs-extra';
+import { sep } from 'path'
 import { PDFDocument } from 'pdf-lib'
-const PDF2Pic = require("pdf2pic");
+import convertapiPackage from 'convertapi';
+const convertapi = convertapiPackage('jmZJT3WkdWUjV1oc');
 
 export async function convertPdf(pathToPdf) {
     const pdfSize = await getPdfSize(pathToPdf);
-
-    const pdf2pic = new PDF2Pic({
-        savedir: "./images",    // output file location
-        format: "jpeg"         // output file format
-    });
-
-    pdf2pic.convert(pathToPdf)
-        .then((resolve) => {
-            console.log("image converter successfully!");
-
-            return resolve;
-        })
-
     console.log(`Width: ${pdfSize.width}px | Height: ${pdfSize.height}px`);
+
+    let conf = {
+        File: pathToPdf,
+        JpgQuality: '100',
+        FileName: 'slide'
+    }
+
+    convertapi.convert('png', conf, 'pdf')
+        .then(result => {
+            console.log("Converted file url: " + result.file.url);
+            return result.saveFiles(process.cwd() + sep + 'images');
+        })
+        .then(filesPath => {
+            console.log("Files path: " + filesPath);
+        })
+        .catch(e => {
+            console.error(e.toString());
+        });
 }
 
 async function getPdfSize(pathToPdf) {
